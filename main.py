471b461118790c68d8091867b1bb05181d5b80b9
kkym-develops <khulaydymianoor@gmail.com>
2026-09-19 13:21:44 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -621,4 +621,5 @@ def document_checker():
     return render_page(html, report=report)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
